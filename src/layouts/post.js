4958f6cfff03c0e15d6171a18b9aff4ba2e40cf4
Stackbit Code Editor <projects@stackbit.com>
2021-07-03 12:43:53 +0000

    post.js: changed

--- src/layouts/post.js
+++ src/layouts/post.js
@@ -8,18 +8,6 @@ import { htmlToReact, withPrefix, markdownify } from '../utils';
 
 export default class Post extends React.Component {
     slug = ""
-    
-    componentDidMount() {
-        firestore.collection('comments').get().then(data => {
-            const newComments = data.docs.filter(doc => doc.data().slug == this.slug).map(item=>{
-                return {id: item.id, ...item.data()}
-            });
-            this.setState({
-                realComments: newComments
-            });
-
-        })
-    } 
 
     render() {
         const data = _.get(this.props, 'data');
