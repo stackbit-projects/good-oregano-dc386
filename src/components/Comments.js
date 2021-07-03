788f5995428f50d2c5be5af907f7f35df6080403
Stackbit Code Editor <projects@stackbit.com>
2021-07-03 12:46:59 +0000

    Comments.js: changed

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -15,7 +15,7 @@ const Comments = ({ slug }) => {
     const [comments, setComments] = useState([]);
     useEffect(() => {
         firestore.collection('comments').get().then(data => {
-            const newComments = data.docs.filter(doc => doc.data().slug == this.slug).map(item=>{
+            const newComments = data.docs.filter(doc => doc.data().slug == slug).map(item=>{
                 return {id: item.id, ...item.data()}
             });
             setComments(newComments);
