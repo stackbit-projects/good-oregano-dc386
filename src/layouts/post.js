85adcea2d22164d4e03ffbf31bc286a6bece48df
Stackbit Code Editor <projects@stackbit.com>
2021-07-03 17:10:28 +0000

    post.js: changed

--- src/layouts/post.js
+++ src/layouts/post.js
@@ -35,7 +35,7 @@ export default class Post extends React.Component {
                         </div>
                     )}
                     {markdownContent && <div className="post-content inner-sm">{markdownify(markdownContent)}</div>}
-                    <Comments slug={slug} />
+                    /*<Comments slug={slug} */
                     <footer className="post-meta inner-sm">
                         <time className="published" dateTime={dateTimeAttr}>{formattedDate}</time>
                         <span> {category} </span>
