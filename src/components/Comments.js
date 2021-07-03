6e2b088294b14d6c6c9d5217de08a19de1625050
Stackbit Code Editor <projects@stackbit.com>
2021-07-03 12:45:11 +0000

    Comments.js: changed

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -10,17 +10,6 @@ const CommentList = styled.div`
   }
 `
 
-    componentDidMount() {
-        firestore.collection('comments').get().then(data => {
-            const newComments = data.docs.filter(doc => doc.data().slug == this.slug).map(item=>{
-                return {id: item.id, ...item.data()}
-            });
-            this.setState({
-                realComments: newComments
-            });
-        })
-    } 
-
 const Comments = ({ slug }) => {
     const [comments, setComments] = useState();
     useEffect(() => {
