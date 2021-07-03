82777f997032037a7e3cb27fc107eec4c9668cbe
Stackbit Code Editor <projects@stackbit.com>
2021-07-03 13:02:01 +0000

    Comment.js: changed

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -67,7 +67,7 @@ const Comment = ({ comment, childComments, slug, reloadComments }) => {
   return (
     <CommentBox>
       <SingleComment comment={comment} />
-      {childComments && childComments.map(child =>
+      {childComments && childComments.sort().map(child =>
         <CommentBox key={child.id} child className="comment-reply">
             <SingleComment comment={child} />
         </CommentBox>
