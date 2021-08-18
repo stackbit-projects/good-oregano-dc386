cc9a630e6b34342df85aad90c2c0507dd0f50868
Stackbit Code Editor <projects@stackbit.com>
2021-08-18 20:01:23 +0000

    submission-created.js: changed

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -3,19 +3,19 @@ const fetch = require('node-fetch')
 const { EMAIL_TOKEN } = process.env
 exports.handler = async event => {
     console.log(JSON.parse(event.body).payload)
-  const email = JSON.parse(event.body).payload.email
-  console.log(`Recieved a submission: ${email}`)
-  return fetch('https://api.buttondown.email/v1/subscribers', {
-    method: 'POST',
-    headers: {
-      Authorization: `Token ${EMAIL_TOKEN}`,
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ email }),
-  })
-    .then(response => response.json())
-    .then(data => {
-      console.log(`Submitted to Buttondown:\n ${data}`)
+    const email = JSON.parse(event.body).payload.email
+    console.log(`Recieved a submission: ${email}`)
+    return fetch('https://api.buttondown.email/v1/subscribers', {
+        method: 'POST',
+        headers: {
+        Authorization: `Token ${EMAIL_TOKEN}`,
+        'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email }),
     })
-    .catch(error => ({ statusCode: 422, body: String(error) }))
+        .then(response => response.json())
+        .then(data => {
+        console.log(`Submitted to Buttondown:\n ${data}`)
+        })
+        .catch(error => ({ statusCode: 422, body: String(error) }))
 }
