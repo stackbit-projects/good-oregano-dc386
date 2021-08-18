84b72adfc9629dcfba2f47617bf02e1bb2bac590
Stackbit Code Editor <projects@stackbit.com>
2021-08-18 19:56:58 +0000

    submission-created.js: changed

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const fetch = require('node-fetch')
 const { EMAIL_TOKEN } = process.env
 exports.handler = async event => {
+    console.log("Received a submission from a different form.")
   if ("subject" in JSON.parse(event.body).payload){
       console.log("Received a submission from a different form.")
       return
