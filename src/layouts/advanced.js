8001ac254018dabef4ebbf762fb27363f2f0727b
Stackbit Code Editor <projects@stackbit.com>
2021-09-03 18:41:07 +0000

    advanced.js: changed

--- src/layouts/advanced.js
+++ src/layouts/advanced.js
@@ -14,6 +14,7 @@ export default class Advanced extends React.Component {
         const title = _.get(page, 'title');
         const sections = _.get(page, 'sections');
         const pageUrl = getPageUrl(page);
+        sections.
 
         return (
             <Layout page={page} config={config}>
