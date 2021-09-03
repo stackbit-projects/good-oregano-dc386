2707dc2b82d54a260e43b75dc15c638dda9061c4
Stackbit Code Editor <projects@stackbit.com>
2021-09-03 19:29:35 +0000

    Header.js: changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -114,7 +114,7 @@ export default class Header extends React.Component {
         const navLinks = _.get(header, 'nav_links');
         const hasSocial = _.get(header, 'has_social');
         const socialLinks = _.get(header, 'social_links');
-        console.log("**" + pageUrl + "**");        
+        console.log("**" + pageUrl + "**");
         if (pageUrl.toString() == "diagnoseme"){
             var fdisplay = "none;";
         }
