bb6f3a9c53a5a41195f33859288a17b71a1b75ab
Stackbit Code Editor <projects@stackbit.com>
2021-09-03 18:47:05 +0000

    advanced.js: changed

--- src/layouts/advanced.js
+++ src/layouts/advanced.js
@@ -14,7 +14,6 @@ export default class Advanced extends React.Component {
         const title = _.get(page, 'title');
         const sections = _.get(page, 'sections');
         const pageUrl = getPageUrl(page);
-        console.log("hi1!!");
         if (title == "Diagnose Me"){
             console.log("hi");
             components["Header"] = "";
