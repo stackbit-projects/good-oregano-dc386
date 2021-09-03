1334d03e74fe5393fc66122ddd8c79aff180b20d
Stackbit Code Editor <projects@stackbit.com>
2021-09-03 19:03:04 +0000

    Header.js: changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -115,7 +115,7 @@ export default class Header extends React.Component {
         const hasSocial = _.get(header, 'has_social');
         const socialLinks = _.get(header, 'social_links');
         console.log(pageUrl);
-        if (pageUrl == "Diagnose Me"){
+        if (pageUrl == "diagnoseme"){
             console.log("hi");
         }
 
