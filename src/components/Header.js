54a30b68cbbf0fdded51e0cf8c170843e07774b3
Stackbit Code Editor <projects@stackbit.com>
2021-09-03 19:05:33 +0000

    Header.js: changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -121,7 +121,7 @@ export default class Header extends React.Component {
         }
 
         return (
-            <header id="masthead" className={`site-header ${headerBackground}`} style={}>
+            <header id="masthead" className={`site-header ${headerBackground}`} style={style}>
                 <div className="site-header-wrap">
                     <div className="site-header-inside">
                         <div className="site-branding">
