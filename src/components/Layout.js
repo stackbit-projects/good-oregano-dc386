3cf0cb66b4d0c960898285d9df97c2da6cd5b412
Stackbit Code Editor <projects@stackbit.com>
2021-09-03 19:48:17 +0000

    Layout.js: changed

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -61,14 +61,12 @@ export default class Body extends React.Component {
         });
         console.log("**" + pageTitle + "**");
         if (pageTitle.toString() == "Diagnose Me" || pageTitle.toString() == "diagnoseme/follow_us"){
-            console.log("in devlog");
             var fdisplay = "margin-left: 100px;";
         }
         else {
             var fdisplay = "";
         } 
         const display = fdisplay;
-        console.log("**" + display + "**"); 
 
         return (
             <React.Fragment>
